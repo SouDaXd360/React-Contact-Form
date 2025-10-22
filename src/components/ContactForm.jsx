62750e498681b1f7c02419a51602d4ab8b92f713
SouDaXd360 <sd360official@gmail.com>
2025-10-22 16:46:43 +0530

    Frontend validation added

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
-function ContactForm({ buttonText, username, setUsername}){
+function ContactForm({ buttonText, username, setUsername }) {
     //const [username,setUsername] = useState('');
-    const [useremail,setUseremail] = useState('');
-    const [message,setMessage] = useState('');
-    const [subject,setSubject] = useState('');
+    const [useremail, setUseremail] = useState('');
+    const [message, setMessage] = useState('');
+    const [subject, setSubject] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-
+        if (username.trim() === '' || email.trim() === '') {
+            alert('Please fill out both Username and Email fields.');
+            return; 
+        }
         const formData = {
             username: username,
             email: useremail,
@@ -16,8 +19,8 @@ function ContactForm({ buttonText, username, setUsername}){
             message: message,
         };
 
-        try{
-            const response = await fetch('https://soumik-contact-api.onrender.com/api/contact',{
+        try {
+            const response = await fetch('https://soumik-contact-api.onrender.com/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -27,40 +30,40 @@ function ContactForm({ buttonText, username, setUsername}){
 
             const result = await response.json();
 
-            if(response.ok){
+            if (response.ok) {
                 alert('Form submitted successfully!');
-                console.log('Success:' ,result);
+                console.log('Success:', result);
 
             }
-            else{
+            else {
                 alert('Error submitting form.');
                 console.error('Error:', result);
             }
-        } 
-        
-        catch(error) {
+        }
+
+        catch (error) {
             console.error('There was an error sending the data!', error);
             alert('A network error occured.');
         }
     };
 
-    return(
+    return (
         <form action="#" className="contact-form" onSubmit={handleSubmit}>
 
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} name="user_name" placeholder="Soumik Dash"/>
-    
-                <label htmlFor="useremail">Email:</label>
-                <input type="email" id="useremail" value={useremail} onChange={(event) => setUseremail(event.target.value)} required/>
-    
-                <label htmlFor="subject">Subject:</label>
-                <input type="text" id="subject" value={subject} onChange={(event) => setSubject(event.target.value)}required/>
-    
-                <label htmlFor="message">Message:</label>
-                <textarea value={message} onChange={(event) => setMessage(event.target.value)} id="message"  rows="4" placeholder="Your message..."></textarea>
-    
-                
-                <input type="submit" value={buttonText} className="submit-btn"/>
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} name="user_name" placeholder="Soumik Dash" />
+
+            <label htmlFor="useremail">Email:</label>
+            <input type="email" id="useremail" value={useremail} onChange={(event) => setUseremail(event.target.value)} required />
+
+            <label htmlFor="subject">Subject:</label>
+            <input type="text" id="subject" value={subject} onChange={(event) => setSubject(event.target.value)} required />
+
+            <label htmlFor="message">Message:</label>
+            <textarea value={message} onChange={(event) => setMessage(event.target.value)} id="message" rows="4" placeholder="Your message..."></textarea>
+
+
+            <input type="submit" value={buttonText} className="submit-btn" />
         </form>
     );
 
